4ece64bdf209bb390a998e5a6d6e9b44632f3abe
Lilja Maria Stefansdottir <liljamaria@eduroam.nat.ru.is>
2018-09-04 18:43:43 +0000

    Finished

--- max_init.py
+++ max_init.py
@@ -7,3 +7,16 @@ The user repeatedly inputs numbers until a negative value is entered.'''
 # The user continues to input numbers until he puts in a negative value
 # Then the maximum number the user put in prints out
 
+
+
+# Fill in the missing code
+num_int = 0
+max_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+        
+
+print("The maximum is", max_int)  
